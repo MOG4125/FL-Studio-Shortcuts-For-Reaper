bcb187b6ec288cf07d6b7fca6971970d1d0b685a
MOG. <puddycat20@hotmail.com>
2026-08-10 09:47:33 +0100

    Update Wipe_FL_Shortcuts.lua

--- Wipe_FL_Shortcuts.lua
+++ Wipe_FL_Shortcuts.lua
@@ -1,9 +1,3 @@
--- @description Purge and Wipe FL Shortcuts
--- @version 1.0
--- @author YourName
--- @about
---   A panic switch tool to safely clear out imported hotkeys and restore default keybinds.
-
 local confirm = reaper.ShowMessageBox("Are you sure you want to delete and reset all current keyboard shortcuts back to REAPER factory default?", "Purge Shortcuts?", 1)
 
 if confirm == 1 then
